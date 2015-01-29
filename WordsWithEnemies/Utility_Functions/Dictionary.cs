cc767a9cfd12128c7f3f8b207425b1694e5b0e62
Jelani <hill.jelani12@gmail.com>
2015-01-29 00:05:31 -0500

    New Stuff

---
+++ WordsWithEnemies/Utility_Functions/Dictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WordsWithEnemies
+{
+    class Dictionary
+    {
+        private static HashSet<String> words;
+ 
+
+        public static void Initialize()
+        {
+            try
+            {
+                using (StreamReader read = new StreamReader("twl06.txt"))
+                {
+                   String[] temp = read.ReadToEnd().Split(' ');
+                   words = new HashSet<string>(temp);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+        }
+
+        public static Boolean CheckWord()
+        {
+            return true;
+        }
+
+        public static List<String> ReturnWordsForList()
+        {
+            return null;
+        }
+    }
+}
